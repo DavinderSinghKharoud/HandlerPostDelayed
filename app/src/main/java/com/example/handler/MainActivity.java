ba39797872a5a18e6428a7d7636a338260ba56b5
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-02 23:01:58 -0500

    Creating Handler

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void StartRepeating(View view) {
+        handler.postDelayed( mToastRunnable, 5000);
+
+    }
+
+    public void StopRepeating(View view) {
+    }
+
+    private Runnable mToastRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(MainActivity.this, "This is delayed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
 }
